419754f593c6cf29caf513b64faa0a5115c1eada
Alexander "Ace" Olofsson <ace@haxalot.com>
2019-01-26 11:34:55 +0100

    Rethinking configuration sync
    Configuration doesn't seem to make much sense as an event,
    can't figure out a good way to handle that

--- lib/matrix_releasetracker/client.rb
+++ lib/matrix_releasetracker/client.rb
@@ -16,27 +16,15 @@ module MatrixReleasetracker
       }
     }.freeze
 
-    PROVISION_MESSAGE_KEY = 'com.github.ananace.RequestTracker.provision'.freeze
-    PROVISION_MESSAGE_FILTER = {
-      presence: { types: [] },
-      account_data: { types: [] },
-      room: {
-        rooms: [],
-        ephemeral: { types: [] },
-        state: { types: [] },
-        timeline: { types: [ROOM_PROVISION_MESSAGE_KEY] },
-        account_data: { types: [] }
-      }
-    }.freeze
-
     attr_reader :api, :data
 
     def initialize(configuration)
+      @use_sync = configuration.delete(:use_sync) { false }
       @api = MatrixSdk::Api.new configuration.delete(:hs_url), configuration
       @data = {}
 
       begin
-        api.whoami? # TODO: @api.logged_in?
+        @user = api.whoami? # TODO: @api.logged_in?
         reload!
 
         data[:next_batch] = configuration.delete :next_batch if configuration.key? :next_batch
@@ -54,18 +42,37 @@ module MatrixReleasetracker
     end
 
     def reload!
-      @data = [api.sync(timeout: 5.0, set_presence: :offline, filter: ACCOUNT_DATA_FILTER.to_json)].map do |data|
-        data = data[:account_data][:events].find { |ev| ev[:type] == ACCOUNT_DATA_KEY }
-        (data[:content] if data) || {}
-      end.first
+      if !@use_sync &&
+         (api.server_version.name != 'Synapse' ||
+          Gem::Version.new(api.server_version.version) >= Gem::Version.new('0.34.1'))
+        reload_with_get
+      else
+        reload_with_sync
+      end
 
       true
     end
 
     def save!
-      api.set_account_data(api.whoami?[:user_id], ACCOUNT_DATA_KEY, @data)
+      api.set_account_data(@user.user_id, ACCOUNT_DATA_KEY, @data)
 
       true
+    end
+
+    private
+
+    def reload_with_sync
+      @data = [api.sync(timeout: 5.0, set_presence: :offline, filter: ACCOUNT_DATA_FILTER.to_json)].map do |data|
+        data = data[:account_data][:events].find { |ev| ev[:type] == ACCOUNT_DATA_KEY }
+        (data[:content] if data) || {}
+      end.first
+    end
+
+    def reload_with_get
+      @data = api.request(:get, :client_r0, "/user/#{@user.user_id}/account_data/#{ACCOUNT_DATA_KEY}")
+    rescue MatrixSdk::MatrixRequestError => ex
+      return reload_with_sync if ex.code == 404
+      raise ex
     end
   end
 end
