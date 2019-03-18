579f9d28d4994707bc2d37eea3d4effe957a72b3
Alexander Olofsson <ace@haxalot.com>
2019-03-18 23:31:03 +0100

    Clean up last_check handling for the user struct

--- lib/matrix_releasetracker/structs.rb
+++ lib/matrix_releasetracker/structs.rb
@@ -2,12 +2,13 @@ module MatrixReleasetracker
   module Structs
     User = Struct.new(:name, :room, :backend, :persistent_data) do
       def last_check
-        return nil unless persistent_data && persistent_data.key?(:last_check)
-        Time.parse((persistent_data || {})[:last_check])
+        return nil if persistent_data.nil? || persistent_data.dig(:last_check).nil?
+        return persistent_data[:last_check] if persistent_data[:last_check].is_a? Time
+        persistent_data[:last_check] = Time.parse(persistent_data[:last_check])
       end
 
       def last_check=(check)
-        (persistent_data || {})[:last_check] = check
+        persistent_data[:last_check] = check
       end
     end
   end
