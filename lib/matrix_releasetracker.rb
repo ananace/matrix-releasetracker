46f4c746f9d1cd22027b50e3de57623db04a90d4
Alexander "Ace" Olofsson <ace@haxalot.com>
2019-09-06 09:26:27 +0200

    Rename backend file to be correct

--- lib/matrix_releasetracker.rb
+++ lib/matrix_releasetracker.rb
@@ -1,5 +1,5 @@
 require 'matrix_releasetracker/version'
-require 'matrix_releasetracker/backends'
+require 'matrix_releasetracker/backend'
 require 'matrix_releasetracker/client'
 require 'matrix_releasetracker/config'
 require 'matrix_releasetracker/release'
