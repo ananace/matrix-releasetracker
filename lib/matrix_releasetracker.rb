fe64573401ceb9ac4682062e1afc43c2709fe1a3
Alexander "Ace" Olofsson <ace@haxalot.com>
2019-09-22 21:34:05 +0200

    Load sequel when necessary

--- lib/matrix_releasetracker.rb
+++ lib/matrix_releasetracker.rb
@@ -5,6 +5,8 @@ require 'matrix_releasetracker/config'
 require 'matrix_releasetracker/release'
 require 'matrix_releasetracker/structs'
 
+autoload :Sequel, 'sequel'
+
 module MatrixReleasetracker
   module Backends
     autoload :Github, 'matrix_releasetracker/backends/github'
